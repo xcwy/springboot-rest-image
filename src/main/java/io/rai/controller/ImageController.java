59d107b4aece28e676db12b6b1803a0c038a8288
xcwy <1475282015@qq.com>
2016-12-25 20:21:41 +0800

    add getImage method

--- src/main/java/io/rai/controller/ImageController.java
+++ src/main/java/io/rai/controller/ImageController.java
@@ -7,9 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import javax.validation.constraints.NotNull;
-import java.io.File;
-import java.io.IOException;
+import java.awt.image.BufferedImage;
+import java.io.*;
 
 /**
  * Created by rai on 16/12/25.
@@ -17,6 +18,10 @@ import java.io.IOException;
 @RestController
 public class ImageController {
   private static final Logger LOG = LoggerFactory.getLogger(ImageController.class);
+
+  private static final String IMAGE_PATH = "/Users/rai/it/resource/image/";
+
+  private static final String SUFFIX = ".JPG";
 
   @Autowired
   private ImageService imageService;
@@ -26,10 +31,10 @@ public class ImageController {
 
     LOG.debug("file content type : {}", file.getContentType());
     LOG.debug("file name : {}", file.getName());
-    LOG.debug("file orginal name : {}",file.getOriginalFilename());
-    LOG.debug("file size : {}" ,file.getSize());
+    LOG.debug("file orginal name : {}", file.getOriginalFilename());
+    LOG.debug("file size : {}", file.getSize());
 
-    File localFile = new File("/Users/rai/it/resource/image/"+file.getOriginalFilename());
+    File localFile = new File(IMAGE_PATH + file.getOriginalFilename());
     try {
       file.transferTo(localFile);
     } catch (IOException e) {
@@ -39,7 +44,19 @@ public class ImageController {
   }
 
   @GetMapping("/images/{fileName}")
-  public byte[] getImage(@PathVariable("fileName") String fileName) {
-    return null;
+  public byte[] getImage(@PathVariable("fileName") String fileName)
+      throws IOException {
+    String realPath = IMAGE_PATH + fileName + SUFFIX;
+    try {
+      InputStream is = new FileInputStream(realPath);
+      BufferedImage img = ImageIO.read(is);
+      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+      ImageIO.write(img, "jpg", bos);
+      return bos.toByteArray();
+    } catch (FileNotFoundException e) {
+      return null; //todo: return safe photo instead
+    } catch (IOException e) {
+      return null;  //todo: return safe photo instead
+    }
   }
 }
